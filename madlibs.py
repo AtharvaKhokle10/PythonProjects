b89f9f9934353da91892da212421ac8fecedcf9d
Atharva Khokle <54365504+AtharvaKhokle10@users.noreply.github.com>
2021-11-12 14:59:49 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+# youtuber = "Atharva"
+
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlibs = f"Computer programming is so {adj}! It makes me so excited all the time \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlibs)
